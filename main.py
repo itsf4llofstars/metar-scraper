325e1532809a3cb0e3fd604a232df5a1c24ad649
itsf4llofstars <irooted4hal@mailfence.com>
2022-02-18 10:21:52 -0600

    Add Write html text to file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-"""main.py file to scrape the weather dat in the form of a raw
-METAR"""
+"""
+main.py file to scrape the weather data in the form of a raw
+METAR
+"""
 import os
 import re
 import requests
@@ -10,9 +12,18 @@ from bs4 import BeautifulSoup as bs
 os.system("clear")
 
 icao = 'kaln'
-
 AWC_METAR = f"https://www.aviationweather.gov/metar/data?ids={icao}&format=raw&date=&hours=0"
 
 awc_page = requests.get(AWC_METAR)
 awc_html = bs(awc_page.content, features="html.parser")
-print(awc_html)
+awc_string = str(awc_html)
+awc_file = None
+
+try:
+    with open(f"/home/pi/python/metar-scraper/{icao}-metar.txt", 'w') as w:
+        awc_file = w.write(awc_string)
+except FileNotFoundError as fnfe:
+    print(f"{fnfe}")
+except Exception as err:
+    print(f"{err}")
+
