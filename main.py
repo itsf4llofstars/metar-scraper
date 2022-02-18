00e57fb6480e9addf411fc57e50f8375fbbe031f
itsf4llofstars <irooted4hal@mailfence.com>
2022-02-18 11:14:52 -0600

    Document code
    Moved variables to a linear order

--- main.py
+++ main.py
@@ -9,21 +9,29 @@ from bs4 import BeautifulSoup as bs
 
 os.system("clear")
 
-ICAO = "kord"
-AWC_METAR = f"https://www.aviationweather.gov/metar/data?ids={ICAO}&format=raw&date=&hours=0"
+# Address and ID for scraping
+ICAO = "klax"
+AWC_METAR_LINK = f"https://www.aviationweather.gov/metar/data?ids={ICAO}&format=raw&date=&hours=0"
 
-AWC_PAGE = requests.get(AWC_METAR)
+# Requests web site data and scrapes html
+AWC_PAGE = requests.get(AWC_METAR_LINK)
 AWC_HTML = bs(AWC_PAGE.content, features="html.parser")
+
+# Setup constant variables for scraping, pasrsing, and writing text
 AWC_STRING = str(AWC_HTML)
 AWC_FILE = None
+HOURLY_METAR = None
+METAR_FILE_NAME = "metars.txt"
 
+
+# Writes scraped html to text file
 try:
     with open(f"/home/pi/python/metar-scraper/{ICAO}-metar.txt", 'w') as w:
         AWC_FILE = w.write(AWC_STRING)
 except FileNotFoundError as fnfe:
     print(f"{fnfe}")
 
-HOURLY_METAR = None
+# Reads in html scraped text and parses out the metar text line
 try:
     with open(f"/home/pi/python/metar-scraper/{ICAO}-metar.txt", 'r') as r:
         html_text = r.readlines()
@@ -35,9 +43,10 @@ else:
             HOURLY_METAR = line[6:-13]
             break
 
+# Deletes unused text file
 os.unlink(f"{ICAO}-metar.txt")
 
-METAR_FILE_NAME = "metars.txt"
+# Appends the raw METAR text to text file
 try:
     with open(f"/home/pi/python/metar-scraper/{METAR_FILE_NAME}", "a") as append:
         append.write(HOURLY_METAR)
