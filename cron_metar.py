0130b8a142f340f3d3f8d7e4d0f8aba4edaf564c
itsf4llofstars <irooted4hal@mailfence.com>
2022-02-19 16:24:47 -0600

    Add: create cron_metar.py file

---
+++ cron_metar.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""cron-metar.py Script to check, scrape and write METAR data on an
+hourly basis. Used as a cron script.
+"""
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+def main():
+    """main function"""
+    ...
+
+
+if __name__ == "__main__":
+    main()
