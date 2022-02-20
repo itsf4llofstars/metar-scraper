58eb7ee9b6910ca75d8ab5be03eec94c2cb03313
itsf4llofstars <irooted4hal@mailfence.com>
2022-02-19 19:47:26 -0600

    Add: Chnage constants to conform to pylint

--- cron_metar.py
+++ cron_metar.py
@@ -2,10 +2,12 @@
 """cron-metar.py Script to check, scrape and write METAR data on an
 hourly basis. Used as a cron script.
 """
-import requests
-import time
 import re
+import time
+import requests
 from bs4 import BeautifulSoup as bs
+
+# TODO: Create functions for all that can be functions from main
 
 
 def write_metar(file_name, metar) -> None:
@@ -14,44 +16,48 @@ def write_metar(file_name, metar) -> None:
         with open(f"/home/pi/Documents/{file_name}", "a") as append:
             append.write(metar)
             append.write("\n")
-    except OSError as os:
-        print(f"{os}")
+    except OSError as os_err:
+        print(f"{os_err}")
 
 
 def main():
     """main function"""
     # icao id's for airports
-    KALN = "kaln"
-    KSTL = "kstl"
+    kaln = "kaln"
+    kstl = "kstl"
 
-    # Web address to scrape
-    AWC_METAR_KALN = f"https://www.aviationweather.gov/metar/data?ids={KALN}&format=raw&date=&hours=0"
-    AWC_METAR_KSTL = f"https://www.aviationweather.gov/metar/data?ids={KSTL}&format=raw&date=&hours=0"
+    # web address to scrape
+    awc_metar_kaln = (
+        f"https://www.aviationweather.gov/metar/data?ids={kaln}&format=raw&date=&hours=0"
+    )
+    awc_metar_kstl = (
+        f"https://www.aviationweather.gov/metar/data?ids={kstl}&format=raw&date=&hours=0"
+    )
 
     # request web-site data
-    KALN_PAGE = requests.get(AWC_METAR_KALN)
+    kaln_page = requests.get(awc_metar_kaln)
     time.sleep(1.0)
-    KSTL_PAGE = requests.get(AWC_METAR_KSTL)
+    kstl_page = requests.get(awc_metar_kstl)
 
     # Scrape html's
-    KALN_HTML = bs(KALN_PAGE.content, features="html.parser")
-    KSTL_HTML = bs(KSTL_PAGE.content, features="html.parser")
+    kaln_html = bs(kaln_page.content, features="html.parser")
+    kstl_html = bs(kstl_page.content, features="html.parser")
 
     # set constant variables for parsing and writing
-    KALN_FILE = "kaln-metar.txt"
-    KSTL_FILE = "kstl-metar.txt"
+    kaln_file = "kaln-metar.txt"
+    kstl_file = "kstl-metar.txt"
 
     # find the stations icao in the code tag
-    kaln_metar = KALN_HTML.find_all("code", string=re.compile(r"KALN"))[0]
-    kstl_metar = KSTL_HTML.find_all("code", string=re.compile(r"KSTL"))[0]
+    kaln_metar = kaln_html.find_all("code", string=re.compile(r"KALN"))[0]
+    kstl_metar = kstl_html.find_all("code", string=re.compile(r"KSTL"))[0]
 
     # create string from the regex returned tag line
     kaln_metar_str = str(kaln_metar)[6:-7]
     kstl_metar_str = str(kstl_metar)[6:-7]
 
     # append METAR wx to file
-    write_metar(KALN_FILE, kaln_metar_str)
-    write_metar(KSTL_FILE, kstl_metar_str)
+    write_metar(kaln_file, kaln_metar_str)
+    write_metar(kstl_file, kstl_metar_str)
 
 
 if __name__ == "__main__":
