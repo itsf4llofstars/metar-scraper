bbd556873d83e31d03802f581e5978736d27090c
itsf4llofstars <irooted4hal@mailfence.com>
2022-04-27 22:14:50 -0500

    Add functions to get code tags
    Added list comp to write metar to file

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ ICAO = ["kstl", "kaln"]
 ## METAR txt file names list
 METAR_FILES = ["kstl.txt", "kaln.txt"]
 
-## Path to text metar text files
+## Path to write the metar text to
 PATH = "/home/pi/metars/"
 
 
@@ -25,61 +25,31 @@ def get_soup(html_text):
     return bs(html_text.content, features="html.parser")
 
 
+def get_tag(html_text):
+    return html_text.code
+
+
+def write_metar(metar_text, filename):
+    global PATH
+    with open(PATH + filename, "a") as append:
+        append.write(metar_text + "\n")
+
+
+metar_list = []
 index = 0
 while index < len(ICAO):
     ## Web address to be scraped
     AWC_METAR_SITE = f"https://www.aviationweather.gov/metar/data?ids={ICAO[index]}&format=raw&date=&hours=0"
 
-    ## Requests web site data
     awc_page = request_website(AWC_METAR_SITE)
-
-    ## Beautiful Soup scraping of the web-site data
-    # awc_html = bs(awc_page.content, features="html.parser")
     awc_html = get_soup(awc_page)
 
-    ## Sets variable to the line that has the METAR text data
-    code_tag = awc_html.code
+    ## Sets code_tag variable to the html line with the metar
+    code_tag = get_tag(awc_html)
 
-    ## Print debug
-    for metar in code_tag.children:
-        print(metar)
+    ## Sets the metars variable to the raw metar text
+    metars = code_tag.children
+
+    [write_metar(metar, METAR_FILES[index]) for metar in metars]
 
     index += 1
-
-'''
-AWC_STRING = str(AWC_HTML)
-AWC_FILE = None
-HOURLY_METAR = None
-METAR_FILE_NAME = "metars.txt"
-
-
-# Writes scraped html to text file
-try:
-    with open(f"/home/$USER/python/metar-scraper/{ICAO[index]}-metar.txt", 'w') as w:
-        AWC_FILE = w.write(AWC_STRING)
-except FileNotFoundError as fnfe:
-    print(f"{fnfe}")
-
-# Reads in html scraped text and parses out the metar text line
-try:
-    with open(f"/home/$USER/python/metar-scraper/{ICAO[index]}-metar.txt", 'r') as r:
-        html_text = r.readlines()
-except FileNotFoundError as fnfe:
-    print(f"{fnfe}")
-else:
-    for line in html_text:
-        if (ICAO[index].upper() in line) and line.startswith("<code>"):
-            HOURLY_METAR = line[6:-13]
-            break
-
-# Deletes unused text file
-os.unlink(f"/home/$USER/python/metar-scraper/{ICAO[index]}-metar.txt")
-
-# Appends the raw METAR text to text file
-try:
-    with open(f"/home/$USER/python/metar-scraper/{METAR_FILE_NAME}", "a") as append:
-        append.write(HOURLY_METAR)
-        append.write("\n")
-except FileNotFoundError as fnfe:
-    print(f"{fnfe}")
-'''
