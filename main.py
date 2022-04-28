5a3034d6909318fe4812f64d74c5de431b5cfcd1
itsf4llofstars <irooted4hal@mailfence.com>
2022-04-27 19:36:37 -0500

    Change ICAO calls to ICAO[index] for while loop

--- main.py
+++ main.py
@@ -14,21 +14,25 @@ ICAO = ["kstl", "kaln"]
 ## METAR txt file names list
 METAR_FILES = ["kstl.txt", "kaln.txt"]
 
+index = 0
+while index < len(ICAO):
 ## Web address to be scraped
-AWC_METAR_LINK = f"https://www.aviationweather.gov/metar/data?ids={ICAO}&format=raw&date=&hours=0"
+    AWC_METAR_LINK = f"https://www.aviationweather.gov/metar/data?ids={ICAO[index]}&format=raw&date=&hours=0"
 
 ## Requests web site data
-AWC_PAGE = requests.get(AWC_METAR_LINK)
+    AWC_PAGE = requests.get(AWC_METAR_LINK)
 
 ## Beautiful Soup scraping of the web-site data
-AWC_HTML = bs(AWC_PAGE.content, features="html.parser")
+    AWC_HTML = bs(AWC_PAGE.content, features="html.parser")
 
 ## Sets variable to the line that has the METAR text data
-CODE_TAG = AWC_HTML.code
+    CODE_TAG = AWC_HTML.code
 
 ## Print debug
-for metar in CODE_TAG.children:
-    print(metar)
+    for metar in CODE_TAG.children:
+        print(metar)
+
+    index += 1
 
 '''
 AWC_STRING = str(AWC_HTML)
@@ -39,25 +43,25 @@ METAR_FILE_NAME = "metars.txt"
 
 # Writes scraped html to text file
 try:
-    with open(f"/home/$USER/python/metar-scraper/{ICAO}-metar.txt", 'w') as w:
+    with open(f"/home/$USER/python/metar-scraper/{ICAO[index]}-metar.txt", 'w') as w:
         AWC_FILE = w.write(AWC_STRING)
 except FileNotFoundError as fnfe:
     print(f"{fnfe}")
 
 # Reads in html scraped text and parses out the metar text line
 try:
-    with open(f"/home/$USER/python/metar-scraper/{ICAO}-metar.txt", 'r') as r:
+    with open(f"/home/$USER/python/metar-scraper/{ICAO[index]}-metar.txt", 'r') as r:
         html_text = r.readlines()
 except FileNotFoundError as fnfe:
     print(f"{fnfe}")
 else:
     for line in html_text:
-        if (ICAO.upper() in line) and line.startswith("<code>"):
+        if (ICAO[index].upper() in line) and line.startswith("<code>"):
             HOURLY_METAR = line[6:-13]
             break
 
 # Deletes unused text file
-os.unlink(f"/home/$USER/python/metar-scraper/{ICAO}-metar.txt")
+os.unlink(f"/home/$USER/python/metar-scraper/{ICAO[index]}-metar.txt")
 
 # Appends the raw METAR text to text file
 try:
